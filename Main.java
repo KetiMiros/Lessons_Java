f715d8509b0b3e1a26775014d333e7768c342088
Katerina Miroslavskaya <92998292+KetiMiros@users.noreply.github.com>
2023-04-30 16:28:14 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package Lesson_2;
+
+import Lesson_2.task.*;
+import Lesson_2.hw.*;
+
+public class Main {
+    // public static void main(String[] args) {
+    //     System.out.println(Task1.stringC1C2toN(9, '+', '/'));
+    // }
+    public static void main(String[] args) {
+        //Task2.writeToFile();
+        //Task3.write(".");
+        //Hw3.calcWithLog();
+        //Hw1.sortByBubble(Hw1.getIntArray(10)); //передаем длину
+        //Hw2.doWrongDesidionHW2();
+    }
+}
