7ce6a980623324f8fb59d86b20f1da2a5f264f56
Katerina Miroslavskaya <92998292+KetiMiros@users.noreply.github.com>
2023-04-30 16:34:27 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 package Lesson_2;
 
-import Lesson_2.task.*;
+import Lesson_2*;
 import Lesson_2.hw.*;
 
 public class Main {
@@ -8,8 +8,8 @@ public class Main {
     //     System.out.println(Task1.stringC1C2toN(9, '+', '/'));
     // }
     public static void main(String[] args) {
-        //Task2.writeToFile();
-        //Task3.write(".");
+        //Task2_L2.writeToFile();
+        //Task_Ll3.write(".");
         //Hw3.calcWithLog();
         //Hw1.sortByBubble(Hw1.getIntArray(10)); //передаем длину
         //Hw2.doWrongDesidionHW2();
